63bea61b4d5bc3287d6b9da20ef5fc242ec978ac
Digital-cavalry <112815366+Digital-cavalry@users.noreply.github.com>
2022-10-14 18:29:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let counter = 0;
+
+function upvote() {
+ counter  = counter + 1;
+
+ document.getElementById("votes").innerHTML = "Понравилось " +  counter + " диванным трейдерам";
+}
+
+function downvote() {
+ counter  = counter - 1;
+
+ document.getElementById("votes").innerHTML = "Понравилось " +  counter + " диванным трейдерам";
+}
+
+let counter1 = 0;
+
+function upvote1() {
+ counter1  = counter1 + 1;
+
+ document.getElementById("votes1").innerHTML = "Понравилось " +  counter1 + " диванным трейдерам";
+}
+
+function downvote1() {
+ counter1  = counter1 - 1;
+
+ document.getElementById("votes1").innerHTML = "Понравилось " +  counter1 + " диванным трейдерам";
+}
+
+let counter2 = 0;
+
+function upvote2() {
+ counter2  = counter2 + 1;
+
+ document.getElementById("votes2").innerHTML = "Понравилось " +  counter2+ " диванным трейдерам";
+}
+
+function downvote2() {
+ counter2  = counter2 - 1;
+
+ document.getElementById("votes2").innerHTML = "Понравилось " +  counter2 + " диванным трейдерам";
+}
+
+let counter3 = 0;
+
+function upvote3() {
+ counter3  = counter3 + 1;
+
+ document.getElementById("votes3").innerHTML = "Понравилось " +  counter3 + " диванным трейдерам";
+}
+
+function downvote3() {
+ counter3  = counter3 - 1;
+
+ document.getElementById("votes3").innerHTML = "Понравилось " +  counter3 + " диванным трейдерам";
+}
