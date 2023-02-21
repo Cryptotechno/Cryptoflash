9939f2784a94d544024a9853956a9b31ddd80295
Digital-cavalry <rumiantsevanatali@gmail.com>
2023-02-21 22:13:20 +0400

    add card 5

--- script.js
+++ script.js
@@ -1,92 +1,98 @@
+
+
 let counter = 0;
 
 function upvote() {
- counter  = counter + 1;
+    counter = counter + 1;
 
- document.getElementById("votes").innerHTML = "Liked" + counter + "traders";
+    document.getElementById("votes").innerHTML = "Liked" + counter + "traders";
 }
 
 function downvote() {
- counter  = counter - 1;
+    counter = counter - 1;
 
- document.getElementById("votes").innerHTML = "Liked " +  counter + " traders";
+    document.getElementById("votes").innerHTML = "Liked " + counter + " traders";
 }
 
 let counter1 = 0;
 
 function upvote1() {
- counter1  = counter1 + 1;
+    counter1 = counter1 + 1;
 
- document.getElementById("votes1").innerHTML = "Liked " +  counter1 + " traders";
+    document.getElementById("votes1").innerHTML = "Liked " + counter1 + " traders";
 }
 
 function downvote1() {
- counter1 = counter1 - 1;
+    counter1 = counter1 - 1;
 
- document.getElementById("votes1").innerHTML = "Liked " +  counter1 + " traders";
+    document.getElementById("votes1").innerHTML = "Liked " + counter1 + " traders";
 }
 
 let counter2 = 0;
 
 function upvote2() {
- counter2  = counter2 + 1;
+    counter2 = counter2 + 1;
 
- document.getElementById("votes2").innerHTML = "Liked " +  counter2 + " traders";
+    document.getElementById("votes2").innerHTML = "Liked " + counter2 + " traders";
 }
 
 function downvote2() {
- counter2 = counter2 - 1;
+    counter2 = counter2 - 1;
 
- document.getElementById("votes2").innerHTML = "Liked " +  counter2 + " traders";
+    document.getElementById("votes2").innerHTML = "Liked " + counter2 + " traders";
 }
 
 let counter3 = 0;
 
 function upvote3() {
- counter3  = counter3 + 1;
+    counter3 = counter3 + 1;
 
- document.getElementById("votes3").innerHTML = "Liked " +  counter3 + " traders";
+    document.getElementById("votes3").innerHTML = "Liked " + counter3 + " traders";
 }
 
 function downvote3() {
- counter3 = counter3 - 1;
+    counter3 = counter3 - 1;
 
- document.getElementById("votes3").innerHTML = "Liked " +  counter3 + " traders";
+    document.getElementById("votes3").innerHTML = "Liked " + counter3 + " traders";
 }
 
 let counter4 = 0;
 
 function upvote4() {
- counter4  = counter4 + 1;
+    counter4 = counter4 + 1;
 
- document.getElementById("votes4").innerHTML = "Liked " +  counter4 + " traders";
+    document.getElementById("votes4").innerHTML = "Liked " + counter4 + " traders";
 }
 
 function downvote4() {
- counter4 = counter4 - 1;
+    counter4 = counter4 - 1;
 
- document.getElementById("votes4").innerHTML = "Liked " +  counter4 + " traders";
+    document.getElementById("votes4").innerHTML = "Liked " + counter4 + " traders";
 }
 
 let counter5 = 0;
 
 function upvote5() {
- counter5  = counter5 + 1;
+    counter5 = counter5 + 1;
 
- document.getElementById("votes5").innerHTML = "Liked " +  counter5 + " traders";
+    document.getElementById("votes5").innerHTML = "Liked " + counter5 + " traders";
 }
 
 function downvote5() {
- counter5 = counter5 - 1;
+    counter5 = counter5 - 1;
 
- document.getElementById("votes5").innerHTML = "Liked " +  counter5 + " traders";
+    document.getElementById("votes5").innerHTML = "Liked " + counter5 + " traders";
 
 }
 
 
+
 // after page load
-$(function(){
-// initialize tooltips for all elements on the page that have the data-toggle="tooltip" attribute
+$(function () {
+    // initialize tooltips for all elements on the page that have the data-toggle="tooltip" attribute
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+
+
 
